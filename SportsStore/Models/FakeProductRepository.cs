578ac50a4a802ab5c7c68d979edbc718556ae90c
Adam Walter <adamwalter@mail.weber.edu>
2019-02-16 16:17:30 -0700

    Finished and published chapter 11

--- SportsStore/Models/FakeProductRepository.cs
+++ SportsStore/Models/FakeProductRepository.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+
 namespace SportsStore.Models
 {
-    public class FakeProductRepository : IProductRepository
+
+    public class FakeProductRepository /* : IProductRepository */
     {
+
         public IQueryable<Product> Products => new List<Product> {
             new Product { Name = "Football", Price = 25 },
             new Product { Name = "Surf board", Price = 179 },
